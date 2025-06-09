4976c8bf13e2b2fa2572444c7602b75cc7407293
optistdev <dskok13371@gmail.com>
2025-06-09 22:13:51 +0200

    udpate avatar

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -7,7 +7,7 @@ const person = {
     return `${this.firstName} ${this.lastName}`;
   },
   role: "Senior Fullstack & AI Engineer",
-  avatar: "/images/avatar.png",
+  avatar: "/images/avatar.jpg",
   email: "inodaren62@gmail.com",
   location: "Europe/Belgrade", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English", "Serbian"], // optional: Leave the array empty if you don't want to display languages
