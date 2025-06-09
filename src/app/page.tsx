b93a27da089d66a9e693a4ea5ebb27667240b73d
optistdev <dskok13371@gmail.com>
2025-06-09 06:53:48 +0200

    remove blogs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,18 +80,6 @@ export default function Home() {
       <RevealFx translateY="16" delay={0.6}>
         <Projects range={[1, 3]} />
       </RevealFx>
-      {routes["/blog"] && (
-        <Flex fillWidth gap="24" mobileDirection="column">
-          <Flex flex={1} paddingLeft="l" paddingTop="24">
-            <Heading as="h2" variant="display-strong-xs" wrap="balance">
-              Latest from the blog
-            </Heading>
-          </Flex>
-          <Flex flex={3} paddingX="20">
-            <Posts range={[1, 2]} columns="2" />
-          </Flex>
-        </Flex>
-      )}
       {newsletter.display && <Mailchimp newsletter={newsletter} />}
     </Column>
   );
